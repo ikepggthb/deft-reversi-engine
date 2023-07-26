d8926a5e0fb938ffe84522bf77db637a92b4b20d
azure <ikkei1188@gmail.com>
2023-07-27 01:38:40 +0900

    Refactor Board structure and CUI navigation in Othello game
    - Refactor Board structure to use a bitboard representation.
    - Create a new function 'get_piece' to get the state of a specific piece.
    - Create a new function '_get_cow' for future use in Othello game logic.
    - Refactor 'print_board' function for better readability and efficiency.
    - Modify 'input_operation' function to improve error handling.
    - Refactor the 'start' function for more precise CUI navigation.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,14 @@ pub struct Board {
     turns: i32
 }
 
+#[derive(Debug)]
+enum Piece {
+    Black,
+    White,
+    None
+}
+
+const BOARD_SIZE: u32 = 8;
 impl Board {
     fn new() -> Self {
         Board {
@@ -16,72 +24,97 @@ impl Board {
             turns: 0
         }
     }
-    fn get_valid(self) {
-        for row in 0..8 {
-            let mut index: i32 = row * 8;
-            for col in 0..8 {
-                let mask: u64 = 1 << index;
-                if self.black & mask == 0 && self.white & mask == 0 {
-                    
-                    for shift in [-9, -8, -7, -1, 1, 7, 8, 9].iter() {
-                        
-                    }
-                }
-                index += 1;
-            }
-        }
+
+    #[inline]
+    fn get_piece(&self, h: u32, w: u32) -> Piece {
+        let mask: u64 = 1 << h * BOARD_SIZE + w;
+
+        if self.black & mask != 0 {Piece::Black}
+        else if self.white & mask != 0 {Piece::White}
+        else {Piece::None}
+    }
+    
+
+    #[inline]
+    fn _get_cow(&self, h: u32, w: u32, count : u32, d: (u32,u32)) -> Piece{
+        self.get_piece(h + d.0 * count, w + d.1 * count)
     }
 }
 
 mod cui_test {
 
-    use crate::Board;
-    use termion::*;
-    use std::io::{Write, stdout, stdin};
-    use termion::input::TermRead;
-    use termion::raw::IntoRawMode;
-    pub fn print_board(board: &Board){
+    use crate::{Board, Piece};
+    //use termion::*;
+    use std::io::stdin;
+    //use termion::input::TermRead;
+    //use termion::raw::IntoRawMode;
+    pub fn print_board(board: &Board, y_now: u32, x_now: u32){
         println!("black: {}\nwhite: {}", board.black_pieces_count, board.white_pieces_count);
-        for row in 0..8 {
-            let mut index: i32 = row * 8;
-            for col in 0..8 {
+        for y in 0..8 {
+            for x in 0..8 {
+                let value: char = {
+                    if y == y_now && x == x_now {'*'}
+                    else {
+                        match board.get_piece(y, x) {
+                            Piece::Black => 'x',
+                            Piece::White => 'o',
+                            Piece::None  => '.',
+                        }
+                    }
+                };
+                print!("{} ", value);
+            }
+            println!();
+        }
+
+        /*
+        高速化 （思考aiを作るときに参照するために残している。）
+        let mut index: u32 = 0;
+        let now_mask: u64 = 1 << y_now * 8 + x_now;
+        for _ in 0..8 {
+            for _ in 0..8 {
                 let mask: u64 = 1 << index;
                 let value: char = 
-                if board.black & mask != 0 {
-                    'x' 
-                } else if board.white & mask != 0 {
-                    'o'
-                } else {
-                    '.'
-                };
+                    if mask == now_mask {'*'}
+                    else if board.black & mask != 0 {'x'} 
+                    else if board.white & mask != 0 {'o'}
+                    else {'.'};
                 print!("{} ", value);
                 index += 1;
             }
             println!();
         }
+        */
     }
-    pub fn start(){
-        let board = Board::new();
-        let mut count = 0; 
+
+    fn input_operation() -> char {
         let mut input = String::new();
         loop {
-            input.clear();
-            print_board(&board);
             stdin().read_line(&mut input).expect("Faild!");
-            let inputed_char = input.trim().chars().next().unwrap();
-
-            match stdin.keys() {
-                'w' => (),
-                'a' => (),
-                's' => (),
-                'd' => (),
-                'q' => return,
-                 x => println!("inputed: {}", x),
+            match input.trim().chars().next() {
+                Some(inputed_char) => return inputed_char,
+                None => input.clear()
             }
         }
     }
-}
+    
+    pub fn start(){
+        let board = Board::new();
+        let (mut y_now, mut x_now): (i32, i32) = (0, 0);
+        loop {
+            print_board(&board, y_now as u32, x_now as u32);
+            match input_operation() {
+                'w' => if y_now == 0 {y_now = 7} else {y_now-=1},
+                'a' => if x_now == 0 {x_now = 7} else {x_now-=1},
+                's' => if y_now == 7 {y_now = 0} else {y_now+=1},
+                'd' => if x_now == 7 {x_now = 0} else {x_now+=1},
+                'q' => return,
+                _ => (),
+            }
 
+        }
+    }
+}
 
 fn main() {
     cui_test::start();
