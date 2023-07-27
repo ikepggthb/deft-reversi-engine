030d5ac56aa5f2ee89c532651e3b1987bfc460f9
azure <ikkei1188@gmail.com>
2023-07-27 17:51:04 +0900

    Implemented basic Othello game logic in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
-pub struct Board {
-    black: u64,
-    white: u64,
-    black_pieces_count: usize,
-    white_pieces_count: usize,
-    turns: i32
-}
+
 
 #[derive(Debug)]
 enum Piece {
@@ -12,8 +6,44 @@ enum Piece {
     White,
     None
 }
+pub struct Board {
+    black: u64,
+    white: u64,
+    black_pieces_count: usize,
+    white_pieces_count: usize,
+    turns: Piece
+}
 
-const BOARD_SIZE: u32 = 8;
+
+const BOARD_SIZE: i32 = 8;
+
+pub struct Coord {
+    x: i32,
+    y: i32
+} 
+
+/*
+todo : 演算子オーバーロード
+impl Coord {
+    fn add(self, ) {
+
+    }
+}
+*/
+
+const DIRECTION:[Coord; 8] = [
+    Coord {x: 1, y: -1},
+    Coord {x: 1, y: 0},
+    Coord {x: 1, y: 1},
+    Coord {x: 0, y: 1},
+    Coord {x: 0, y: -1},
+    Coord {x: -1, y: 0},
+    Coord {x: -1, y: 1},
+    Coord {x: -1, y: -1},
+    ];
+
+
+
 impl Board {
     fn new() -> Self {
         Board {
@@ -21,22 +51,66 @@ impl Board {
             white: 0x0000001008000000u64,
             black_pieces_count: 2usize,
             white_pieces_count: 2usize,
-            turns: 0
+            turns: Piece::Black
         }
     }
 
     #[inline]
-    fn get_piece(&self, h: u32, w: u32) -> Piece {
-        let mask: u64 = 1 << h * BOARD_SIZE + w;
+    fn get_piece(&self, y: i32, x: i32) -> Piece {
+        let mask: u64 = 1 << y * BOARD_SIZE + x;
+
+        let blank = !(self.black| self.white);
 
         if self.black & mask != 0 {Piece::Black}
         else if self.white & mask != 0 {Piece::White}
         else {Piece::None}
     }
-    
+    fn put_piece(&mut self, y: i32, x: i32) {
+        let mask = 1 << y * BOARD_SIZE + x;
+        match self.turns {
+            Piece::Black => {
+                self.black += mask;
+                self.turns = Piece::White;
+            },
+            Piece::White => {
+                self.white += mask;
+                self.turns = Piece::Black;
+            },
+            Piece::None => ()
+        }
+    }
+
+    fn put_able(&self, coord: Coord) -> bool{
+        let mut mask = 0xFEFEFEFEFEFEFE; // 左端が0
+        /* 0xFEFEFEFEFEFEFE 
+        1 1 1 1 1 1 1 0
+        1 1 1 1 1 1 1 0
+        1 1 1 1 1 1 1 0
+        1 1 1 1 1 1 1 0
+        1 1 1 1 1 1 1 0
+        1 1 1 1 1 1 1 0
+        1 1 1 1 1 1 1 0
+        1 1 1 1 1 1 1 0
+        */
+        
+        let blank = !(self.black | self.white);
+
+
+        // black_left
+        // 合法手の調べ方（bit board）
+        // https://speakerdeck.com/antenna_three/bitutobodojie-shuo?slide=44
+        let left = (self.black << 1) & mask; 
+        let mut left_white = left & self.white;
+        for _ in 0..6 {
+            left_white |= (left_white << 1) & mask & self.white;
+        }
+        let mut legal = (left_white << 1) & mask & blank;
+
+        true
+    }legal
 
     #[inline]
-    fn _get_cow(&self, h: u32, w: u32, count : u32, d: (u32,u32)) -> Piece{
+    fn _get_cow(&self, h: i32, w: i32, count : i32, d: (i32,i32)) -> Piece{
         self.get_piece(h + d.0 * count, w + d.1 * count)
     }
 }
@@ -48,7 +122,8 @@ mod cui_test {
     use std::io::stdin;
     //use termion::input::TermRead;
     //use termion::raw::IntoRawMode;
-    pub fn print_board(board: &Board, y_now: u32, x_now: u32){
+
+    pub fn print_board(board: &Board, y_now: i32, x_now: i32){
         println!("black: {}\nwhite: {}", board.black_pieces_count, board.white_pieces_count);
         for y in 0..8 {
             for x in 0..8 {
@@ -99,16 +174,18 @@ mod cui_test {
     }
     
     pub fn start(){
-        let board = Board::new();
+        let mut board = Board::new();
         let (mut y_now, mut x_now): (i32, i32) = (0, 0);
         loop {
-            print_board(&board, y_now as u32, x_now as u32);
+            print_board(&board, y_now, x_now);
             match input_operation() {
                 'w' => if y_now == 0 {y_now = 7} else {y_now-=1},
                 'a' => if x_now == 0 {x_now = 7} else {x_now-=1},
                 's' => if y_now == 7 {y_now = 0} else {y_now+=1},
                 'd' => if x_now == 7 {x_now = 0} else {x_now+=1},
+                'x' => board.put_piece(y_now, x_now),
                 'q' => return,
+                'e' => return,
                 _ => (),
             }
 
@@ -118,4 +195,7 @@ mod cui_test {
 
 fn main() {
     cui_test::start();
+
+
+    let a:[ Coord; 2] = [Coord { x: 0, y : 4}, Coord{x: 1, y: 0}];
 }
