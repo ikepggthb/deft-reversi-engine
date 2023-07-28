34b9b7ae19f2065e489e6add377bbe78b1093b77
azure <ikkei1188@gmail.com>
2023-07-28 14:57:42 +0900

    Add reverse_piece method to Board struct for efficient piece flipping

--- src/main.rs
+++ src/main.rs
@@ -1,127 +1,142 @@
 
-
 #[derive(Debug)]
-enum Piece {
-    Black,
-    White,
-    None
-}
+
 pub struct Board {
     black: u64,
     white: u64,
     black_pieces_count: usize,
     white_pieces_count: usize,
-    turns: Piece
+    turns: u32
 }
-
-
-const BOARD_SIZE: i32 = 8;
-
-pub struct Coord {
-    x: i32,
-    y: i32
-} 
-
-/*
-todo : 演算子オーバーロード
-impl Coord {
-    fn add(self, ) {
-
-    }
-}
-*/
-
-const DIRECTION:[Coord; 8] = [
-    Coord {x: 1, y: -1},
-    Coord {x: 1, y: 0},
-    Coord {x: 1, y: 1},
-    Coord {x: 0, y: 1},
-    Coord {x: 0, y: -1},
-    Coord {x: -1, y: 0},
-    Coord {x: -1, y: 1},
-    Coord {x: -1, y: -1},
-    ];
-
 
 
 impl Board {
+
+    const BOARD_SIZE: i32 = 8;
+    const BLACK_TURN: u32 = 0;
+    const WHITE_TURN: u32 = 1;
+
     fn new() -> Self {
         Board {
             black: 0x0000000810000000u64,
             white: 0x0000001008000000u64,
             black_pieces_count: 2usize,
             white_pieces_count: 2usize,
-            turns: Piece::Black
+            turns: 0
         }
     }
 
     #[inline]
-    fn get_piece(&self, y: i32, x: i32) -> Piece {
-        let mask: u64 = 1 << y * BOARD_SIZE + x;
-
-        let blank = !(self.black| self.white);
-
-        if self.black & mask != 0 {Piece::Black}
-        else if self.white & mask != 0 {Piece::White}
-        else {Piece::None}
-    }
     fn put_piece(&mut self, y: i32, x: i32) {
-        let mask = 1 << y * BOARD_SIZE + x;
-        match self.turns {
-            Piece::Black => {
-                self.black += mask;
-                self.turns = Piece::White;
-            },
-            Piece::White => {
-                self.white += mask;
-                self.turns = Piece::Black;
-            },
-            Piece::None => ()
+        let mask = 1 << y * Board::BOARD_SIZE + x;
+
+        if self.put_able() & mask == 0 {
+            println!("そこには置けません！");
+            return;
         }
+        if self.turns == Board::BLACK_TURN {
+            self.black |= mask;
+        }else {
+            self.white |= mask;
+        }
+        self.reverse_piece(mask);
+        self.turns = self.turns ^ 1;   
     }
 
-    fn put_able(&self, coord: Coord) -> bool{
-        let mut mask = 0xFEFEFEFEFEFEFE; // 左端が0
-        /* 0xFEFEFEFEFEFEFE 
-        1 1 1 1 1 1 1 0
-        1 1 1 1 1 1 1 0
-        1 1 1 1 1 1 1 0
-        1 1 1 1 1 1 1 0
-        1 1 1 1 1 1 1 0
-        1 1 1 1 1 1 1 0
-        1 1 1 1 1 1 1 0
-        1 1 1 1 1 1 1 0
-        */
+    fn reverse_piece(&mut self, put_mask: u64) {
+        let directions: [i32; 4] = [1, 8, 7, 9];
+        let masks: [u64; 4] = [
+            0x7e7e7e7e7e7e7e7e, // 左右
+            0xffffffffffffff00, // 上下
+            0x007e7e7e7e7e7e00, // 斜め
+            0x007e7e7e7e7e7e00, // 斜め
+        ];
+
+        let player_board: u64;
+        let opponent_board: u64;
+        if self.turns == Board::BLACK_TURN {
+            player_board = self.black;
+            opponent_board = self.white;
+        }else {
+            player_board = self.white;
+            opponent_board = self.black;
+        }
+
+        let mut reverse_board = 0u64;
+        for (&direction, &mask) in directions.iter().zip(&masks) {
+            let mut shifted_bit = put_mask << direction & mask;
+            let mut prev_shifted_bit= 0u64;
+            while shifted_bit & opponent_board != 0u64 {
+                prev_shifted_bit |= shifted_bit;
+                shifted_bit = shifted_bit << direction & mask;
+            }
+            if shifted_bit & player_board != 0 {
+                reverse_board |= prev_shifted_bit;
+            }
+
+            // 逆方向
+            let mut shifted_bit = put_mask >> direction & mask;
+            let mut prev_shifted_bit = 0u64;
+            while shifted_bit & opponent_board != 0u64 {
+                prev_shifted_bit |= shifted_bit;
+                shifted_bit = shifted_bit >> direction & mask;
+            }
+            if shifted_bit & player_board != 0 {
+                reverse_board |= prev_shifted_bit;
+            }
+        }
         
+        self.white ^= reverse_board;
+        self.black ^= reverse_board;
+    }
+
+    #[inline]
+    fn put_able(&self) -> u64{
         let blank = !(self.black | self.white);
 
+        let directions: [i32; 4] = [1, 8, 7, 9];
+        let masks: [u64; 4] = [
+            0x7e7e7e7e7e7e7e7e, // 左右
+            0xffffffffffffff00, // 上下
+            0x007e7e7e7e7e7e00, // 斜め
+            0x007e7e7e7e7e7e00, // 斜め
+        ];
 
-        // black_left
-        // 合法手の調べ方（bit board）
-        // https://speakerdeck.com/antenna_three/bitutobodojie-shuo?slide=44
-        let left = (self.black << 1) & mask; 
-        let mut left_white = left & self.white;
-        for _ in 0..6 {
-            left_white |= (left_white << 1) & mask & self.white;
+        let player_board: u64;
+        let opponent_board: u64;
+        if self.turns == Board::BLACK_TURN {
+            player_board = self.black;
+            opponent_board = self.white;
+        }else {
+            player_board = self.white;
+            opponent_board = self.black;
         }
-        let mut legal = (left_white << 1) & mask & blank;
 
-        true
-    }legal
+        let mut legal_moves = 0u64;
+        for (direction, mask) in directions.iter().zip(&masks) {
+            let mut flipped_positions =  (player_board << *direction) & *mask & opponent_board;
+            for _ in 0..5 { // 5回の反復で8マス全てをカバーできます。
+                flipped_positions |=  (flipped_positions << *direction) & *mask & opponent_board;
+            }
+            legal_moves |=  (flipped_positions << *direction) & blank;
+            let mut flipped_positions =  (player_board >> *direction) & *mask & opponent_board;
+            for _ in 0..5 { // 5回の反復で8マス全てをカバーできます。
+                flipped_positions |=  (flipped_positions >> *direction) & *mask & opponent_board;
+            }
+            legal_moves |=  (flipped_positions >> *direction) & blank;
+        }
 
-    #[inline]
-    fn _get_cow(&self, h: i32, w: i32, count : i32, d: (i32,i32)) -> Piece{
-        self.get_piece(h + d.0 * count, w + d.1 * count)
+        legal_moves
+
     }
+
+
 }
 
 mod cui_test {
 
-    use crate::{Board, Piece};
-    //use termion::*;
+    use crate::Board;
     use std::io::stdin;
-    //use termion::input::TermRead;
-    //use termion::raw::IntoRawMode;
 
     pub fn print_board(board: &Board, y_now: i32, x_now: i32){
         println!("black: {}\nwhite: {}", board.black_pieces_count, board.white_pieces_count);
@@ -129,37 +144,20 @@ mod cui_test {
             for x in 0..8 {
                 let value: char = {
                     if y == y_now && x == x_now {'*'}
-                    else {
-                        match board.get_piece(y, x) {
-                            Piece::Black => 'x',
-                            Piece::White => 'o',
-                            Piece::None  => '.',
-                        }
+                        else {
+                        let mask: u64 = 1 << y * 8 + x;
+                        let put_able_bit = board.put_able();
+
+                        if put_able_bit & mask != 0 {'#'}
+                        else if board.black & mask != 0 {'x'}
+                        else if board.white & mask != 0 {'o'}
+                        else {'.'}
                     }
                 };
                 print!("{} ", value);
             }
             println!();
         }
-
-        /*
-        高速化 （思考aiを作るときに参照するために残している。）
-        let mut index: u32 = 0;
-        let now_mask: u64 = 1 << y_now * 8 + x_now;
-        for _ in 0..8 {
-            for _ in 0..8 {
-                let mask: u64 = 1 << index;
-                let value: char = 
-                    if mask == now_mask {'*'}
-                    else if board.black & mask != 0 {'x'} 
-                    else if board.white & mask != 0 {'o'}
-                    else {'.'};
-                print!("{} ", value);
-                index += 1;
-            }
-            println!();
-        }
-        */
     }
 
     fn input_operation() -> char {
@@ -196,6 +194,4 @@ mod cui_test {
 fn main() {
     cui_test::start();
 
-
-    let a:[ Coord; 2] = [Coord { x: 0, y : 4}, Coord{x: 1, y: 0}];
 }
