563fa16adbfc999858f88c2a07e4b7cbee82b750
ikepggthb <ikeprg@gmail.com>
2023-09-13 23:18:09 +0900

    update: enhancement of evaluation function

--- src/ai.rs
+++ src/ai.rs
@@ -1,58 +1,7 @@
 
 use crate::board::*;
 use rand::Rng;
-static mut tcount: i64 = 0;
-
-// pub fn end_game_full_solver(board: &Board) -> u64{
-//     let mut moves = board.put_able();
-//     if moves == 0 {
-//         return 0;
-//     }
-
-//     let dfs = |first_board: Board, now_turns: usize| -> i32{
-
-//         let mut min_score = 64;
-
-//         let mut stack: Vec<Board> = Vec::new();
-//         stack.push(first_board);
-
-//         while let Some(mut current_board) = stack.pop() {
-//             let mut moves = current_board.put_able();
-//             if moves == 0 {
-//                 current_board.next_turn ^= 1;
-//                 moves = current_board.put_able();
-//                 if moves == 0 {
-//                     min_score = min_score.min(current_board.bit_board[now_turns].count_ones() as i32 - current_board.bit_board[now_turns ^ 1].count_ones() as i32);
-//                 }
-//             }
-//             while  moves != 0 {
-//                 let mut virt_board = current_board.clone();
-//                 let put_place = (!moves + 1) & moves; //最も小さい位のbitをマスクする
-//                 moves &= moves - 1; // 最も小さい位のbitを消す
-//                 virt_board.put_piece(put_place);
-//                 stack.push(virt_board);
-//             }
-//         }
-//         min_score
-//     };
-    
-//     let mut max_score = -64;
-//     let mut max_score_move = 0u64;
-//     while  moves != 0 {
-//         let mut virt_board = board.clone();
-//         let put_place = (!moves + 1) & moves; //最も小さい位のbitをマスクする
-//         moves &= moves - 1; // 最も小さい位のbitを消す
-//         virt_board.put_piece(put_place);
-//         let this_score = dfs(virt_board.clone(), board.next_turn);
-//         if this_score > max_score {
-//             max_score = this_score;
-//             max_score_move = put_place;
-//         }
-
-//     }
-//     eprintln!("full solver: {}", max_score);
-//     max_score_move
-// } 
+pub static mut tcount: i64 = 0;
 
 pub fn end_game_full_solver_negamax(board: &Board) -> u64{
     let mut moves = board.put_able();
@@ -80,41 +29,6 @@ pub fn end_game_full_solver_negamax(board: &Board) -> u64{
     eprintln!("full solver: {}", max_score);
     max_score_move
 } 
-
-pub fn nega_alpha(board: &mut Board, alpha: i32, beta: i32) -> i32{
-
-    // 探索範囲: [alpha, beta]
-    let mut moves = board.put_able();
-    let mut best_score = i32::MIN;
-    unsafe {tcount += 1;}
-    while moves != 0 {
-        let mut current_board = board.clone();
-        let put_place = (!moves + 1) & moves;
-        moves &= moves - 1;
-        current_board.put_piece_fast(put_place);
-        let score = -nega_alpha(&mut current_board, alpha, beta);
-        best_score = best_score.max(score);
-    }
-
-    if best_score == i32::MIN {
-        board.next_turn ^= 1; //pass
-        if board.put_able() == 0 { // passしても置くところがない == ゲーム終了
-            return  board.bit_board[board.next_turn  ^ 1].count_ones() as i32 - board.bit_board[board.next_turn].count_ones() as i32;
-            
-            // ここは、処理を高速化するため、passしたのををもとに戻すを省略していることに注意
-            // 本来であれば以下のようになる
-            // passをもとに戻す
-            //     board.next_turn ^= 1 
-            // 「最後に打った次の評価値」すなわち、「boardの自分の手番の評価値」すなわち、「最後に打った手番の負の評価値」を返す
-            //     return board.bit_board[board.next_turn].count_ones() as i32 - board.bit_board[board.next_turn  ^ 1].count_ones() as i32; 
-            // もちろん、終盤ソルバーなので、最後の石数差を評価値としている。
-        }
-        return -nega_alpha(board, -alpha, -beta);
-    }
-
-    best_score
-}
-
 
 pub fn negamax(board: &mut Board) -> i32{
 
@@ -148,6 +62,173 @@ pub fn negamax(board: &mut Board) -> i32{
 
     best_score
 }
+
+pub fn end_game_full_solver_nega_alpha(board: &Board) -> u64{
+    let mut moves = board.put_able();
+    if moves == 0 {
+        return 0;
+    }
+
+    const score_inf: i32 = 100000i32;
+    let mut alpha = -score_inf;
+    let mut max_score_move = 0u64;
+    let beta = score_inf;
+    
+    eprintln!("my_turn: {}", board.next_turn);
+    unsafe {tcount = 0;}
+    while  moves != 0 {
+        let mut virt_board = board.clone();
+        let put_place = (!moves + 1) & moves; //最も小さい位のbitをマスクする
+        moves &= moves - 1; // 最も小さい位のbitを消す
+        virt_board.put_piece(put_place);
+        let this_score = -nega_alpha(&mut virt_board, -beta, -alpha);
+        eprintln!("this_score: {}",this_score);
+        if this_score > alpha {
+            alpha = this_score;
+            max_score_move = put_place;
+        }
+    }
+    unsafe { eprintln!("searched nodes: {}", tcount);}
+    eprintln!("full solver: {}", alpha);
+    max_score_move
+} 
+
+
+pub fn nega_alpha(board: &mut Board, mut alpha: i32,beta: i32) -> i32{
+
+    // 探索範囲: [alpha, beta]
+    let mut moves = board.put_able();
+    let mut best_score = i32::MIN;
+    unsafe {tcount += 1;}
+
+    
+    while moves != 0 {
+        let mut current_board = board.clone();
+        let put_place = (!moves + 1) & moves;
+        moves &= moves - 1;
+        current_board.put_piece_fast(put_place);
+        let score = -nega_alpha(&mut current_board, -beta, -alpha);
+        if score >= beta {
+            return score;
+        }
+        alpha = alpha.max(score);
+        best_score = best_score.max(score);
+    }
+
+    if best_score == i32::MIN {
+        board.next_turn ^= 1; //pass
+        if board.put_able() == 0 { // passしても置くところがない == ゲーム終了
+            return  board.bit_board[board.next_turn  ^ 1].count_ones() as i32 - board.bit_board[board.next_turn].count_ones() as i32;
+            
+            // ここは、処理を高速化するため、passしたのををもとに戻すを省略していることに注意
+            // 本来であれば以下のようになる
+            // passをもとに戻す
+            //     board.next_turn ^= 1 
+            // 「最後に打った次の評価値」すなわち、「boardの自分の手番の評価値」すなわち、「最後に打った手番の負の評価値」を返す
+            //     return board.bit_board[board.next_turn].count_ones() as i32 - board.bit_board[board.next_turn  ^ 1].count_ones() as i32; 
+            // もちろん、終盤ソルバーなので、最後の石数差を評価値としている。
+        }
+        return -nega_alpha(board, -beta, -alpha);
+    }
+
+    best_score
+}
+
+
+pub fn end_game_full_solver_nega_alpha_move_ordering(board: &Board) -> u64{
+    let mut moves = board.put_able();
+    if moves == 0 {
+        return 0;
+    }
+    const score_inf: i32 = 100000i32;
+    eprintln!("my_turn: {}", board.next_turn);
+    unsafe {tcount = 0;}
+
+    // move ordering
+    let mut put_board: Vec<(i32, Board, u64)> = Vec::with_capacity(moves.count_ones() as usize);
+    while moves != 0 {
+        let put_place = (!moves + 1) & moves;
+        moves &= moves - 1;
+        let mut current_put_board = board.clone();
+        current_put_board.put_piece_fast(put_place);
+        put_board.push((current_put_board.put_able() as i32, current_put_board, put_place));
+    }
+
+    let mut alpha = -score_inf;
+    let beta = score_inf;
+    let mut max_score_move = 0u64;
+    
+    put_board.sort_unstable_by(|(a,_, _), (b, _, _)| a.partial_cmp(b).unwrap());
+
+    
+    for (_,current_put_board, put_place) in put_board.iter_mut() {
+        let score = -nega_alpha_move_ordering(current_put_board, -beta, -alpha);
+        eprintln!("this_score: {}",score);
+        if score > alpha {
+            alpha = score;
+            max_score_move = *put_place;
+        }
+    }
+
+    unsafe { eprintln!("searched nodes: {}", tcount);}
+    eprintln!("full solver: {}", alpha);
+    max_score_move
+} 
+
+
+pub fn nega_alpha_move_ordering(board: &mut Board, mut alpha: i32,beta: i32) -> i32{
+
+    // 探索範囲: [alpha, beta]
+    let mut moves = board.put_able();
+    unsafe {tcount += 1;}
+
+    if board.bit_board[Board::BLACK].count_ones() + board.bit_board[Board::WHITE].count_ones() >= 58  {
+        unsafe {tcount -= 1;}
+        return nega_alpha(board, alpha, beta);
+    }
+
+    // move ordering
+    let mut put_board: Vec<(i32, Board)> = Vec::with_capacity(moves.count_ones() as usize);
+    while moves != 0 {
+        let put_place = (!moves + 1) & moves;
+        moves &= moves - 1;
+        let mut current_put_board = board.clone();
+        current_put_board.put_piece_fast(put_place);
+        put_board.push((current_put_board.put_able() as i32, current_put_board));
+
+    }
+
+    put_board.sort_unstable_by(|(a,_), (b, _)| a.partial_cmp(b).unwrap());
+    
+    let mut best_score = i32::MIN;
+    for (_,current_put_board) in put_board.iter_mut() {
+        let score = -nega_alpha_move_ordering(current_put_board, -beta, -alpha);
+        if score >= beta {
+            return score;
+        }
+        alpha = alpha.max(score);
+        best_score = best_score.max(score);
+    }
+
+    if best_score == i32::MIN {
+        board.next_turn ^= 1; //pass
+        if board.put_able() == 0 { // passしても置くところがない == ゲーム終了
+            return  board.bit_board[board.next_turn  ^ 1].count_ones() as i32 - board.bit_board[board.next_turn].count_ones() as i32;
+            
+            // ここは、処理を高速化するため、passしたのををもとに戻すを省略していることに注意
+            // 本来であれば以下のようになる
+            // passをもとに戻す
+            //     board.next_turn ^= 1 
+            // 「最後に打った次の評価値」すなわち、「boardの自分の手番の評価値」すなわち、「最後に打った手番の負の評価値」を返す
+            //     return board.bit_board[board.next_turn].count_ones() as i32 - board.bit_board[board.next_turn  ^ 1].count_ones() as i32; 
+            // もちろん、終盤ソルバーなので、最後の石数差を評価値としている。
+        }
+        return -nega_alpha_move_ordering(board, -beta, -alpha);
+    }
+
+    best_score
+}
+
 
 pub fn put_random_piece(board: &mut Board) -> Result<(), PutPieceErr> {
     let legal_moves = board.put_able();
@@ -208,14 +289,14 @@ pub fn put_eval_zero_simple (board: &mut Board) -> Result<(), PutPieceErr> {
 
 pub fn simplest_eval (board: &Board, turn: usize) -> i32 {
     const SCORES: [i32; 64] = [
-        120, -20, 20,  5,  5, 20, -20, 120,
-        -20, -40, -5, -5, -5, -5, -40, -20,
+        120, -40, 20, 10, 10, 20, -40, 120,
+        -40, -60, -5, -5, -5, -5, -60, -40,
          20,  -5, 15,  3,  3, 15,  -5,  20,
-          5,  -5,  3,  3,  3,  3,  -5,   5,
-          5,  -5,  3,  3,  3,  3,  -5,   5,
+         10,  -5,  3,  3,  3,  3,  -5,  10,
+         10,  -5,  3,  3,  3,  3,  -5,  10,
          20,  -5, 15,  3,  3, 15,  -5,  20,
-        -20, -40, -5, -5, -5, -5, -40, -20,
-        120, -20, 20,  5,  5, 20, -20, 120,
+        -40, -60, -5, -5, -5, -5, -60, -40,
+        120, -40, 20, 10, 10, 20, -40, 120,
     ];
 
     let mut score = 0;
@@ -232,7 +313,15 @@ pub fn simplest_eval (board: &Board, turn: usize) -> i32 {
         score -= SCORES[bit_index];
     }
 
-    score
+    let mut board_for_m = board.clone();
+    // let mask = 0b10111101_00111100_11111111_11111111_11111111_11111111_00111100_10111101_u64;
+    let mask = 0b11111111_10111101_11111111_11111111_11111111_11111111_10111101_11111111_u64;
+    let opponent_mobility = (board_for_m.put_able() & mask).count_ones() as i32;
+    board_for_m.next_turn = board_for_m.next_turn ^ 1;
+    let player_mobility = (board_for_m.put_able() & mask).count_ones() as i32;
+    board_for_m.next_turn = board_for_m.next_turn ^ 1;
+    score + (player_mobility - opponent_mobility) * 7
+
 }
 
 pub fn put_eval_one_simple (board: &mut Board) -> Result<(), PutPieceErr> {
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 extern crate termion;
-use std::io::{stdout, Write, stdin};
-use termion::*;
+use std::io::{stdin, stdout, Write};
 use termion::event::{Event, Key};
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use termion::screen::AlternateScreen;
-
+use termion::*;
 
 mod ai;
 use ai::*;
@@ -25,7 +24,7 @@ fn init_terminal(output: &mut TermOut) -> std::io::Result<()> {
 enum TitleScreenOption {
     Start,
     Exit,
-    None
+    None,
 }
 
 const TITLE: &str = "Deft Reversi";
@@ -35,32 +34,34 @@ struct TitleScreenObject {
     name: String,
     x: i32,
     y: i32,
-    label : String,
-    option: TitleScreenOption
+    label: String,
+    option: TitleScreenOption,
 }
 
-fn title_screen(output: &mut TermOut, input: &mut std::io::Stdin) -> std::io::Result<TitleScreenOption> {
-
+fn title_screen(
+    output: &mut TermOut,
+    input: &mut std::io::Stdin,
+) -> std::io::Result<TitleScreenOption> {
     let title_label = TitleScreenObject {
         name: "title label".to_string(),
         x: 1,
         y: 1,
         label: TITLE.to_string(),
-        option: TitleScreenOption::None
+        option: TitleScreenOption::None,
     };
     let game_start_button = TitleScreenObject {
         name: "start button".to_string(),
         x: 1,
         y: 3,
         label: "Game Start".to_string(),
-        option: TitleScreenOption::Start
+        option: TitleScreenOption::Start,
     };
     let exit_button = TitleScreenObject {
         name: "exit button".to_string(),
         x: 4,
         y: 5,
         label: "Exit".to_string(),
-        option: TitleScreenOption::Exit
+        option: TitleScreenOption::Exit,
     };
 
     let title_object = [&title_label, &game_start_button, &exit_button];
@@ -70,20 +71,21 @@ fn title_screen(output: &mut TermOut, input: &mut std::io::Stdin) -> std::io::Re
     for evt in input.events() {
         match evt? {
             // Ctrl-cでプログラム終了
-            Event::Key(Key::Ctrl('c')) | Event::Key(Key::Char('q')) =>  {
+            Event::Key(Key::Ctrl('c')) | Event::Key(Key::Char('q')) => {
                 return Ok(TitleScreenOption::Exit);
             }
-            Event::Key(Key::Char('\r')) | Event::Key(Key::Char('\n')) =>  { // Enter Key
-                if let TitleScreenOption::None = title_object[title_cursor as usize].option{
+            Event::Key(Key::Char('\r')) | Event::Key(Key::Char('\n')) => {
+                // Enter Key
+                if let TitleScreenOption::None = title_object[title_cursor as usize].option {
                     continue;
                 }
                 return Ok(title_object[title_cursor as usize].option.clone());
             }
             Event::Key(Key::Char(key_char)) => {
                 match key_char {
-                    'w' | 'a' => title_cursor-=1,
-                    's' | 'd' => title_cursor+=1,
-                    _ => ()
+                    'w' | 'a' => title_cursor -= 1,
+                    's' | 'd' => title_cursor += 1,
+                    _ => (),
                 }
                 while title_cursor < 0 {
                     title_cursor += title_object.len() as i32;
@@ -91,55 +93,77 @@ fn title_screen(output: &mut TermOut, input: &mut std::io::Stdin) -> std::io::Re
                 title_cursor = title_cursor % title_object.len() as i32;
                 print_title_screen(output, &title_object, title_cursor)?;
             }
-            _ => ()
+            _ => (),
         }
     }
 
     Ok(TitleScreenOption::Exit)
 }
 
-fn print_title_screen(output: &mut TermOut, title_object: &[&TitleScreenObject], title_cursor: i32 )-> std::io::Result<()>{
+fn print_title_screen(
+    output: &mut TermOut,
+    title_object: &[&TitleScreenObject],
+    title_cursor: i32,
+) -> std::io::Result<()> {
     init_terminal(output)?;
 
     for y in 0..8 {
         for x in 0..8 {
             write!(output, " ")?;
-            for (i,ob) in title_object.iter().enumerate() {
+            for (i, ob) in title_object.iter().enumerate() {
                 if ob.x == x && ob.y == y {
                     write!(output, "{}", ob.label)?;
-                    if title_cursor  as usize == i {
+                    if title_cursor as usize == i {
                         write!(output, "  <-")?;
                     }
                 }
-                
             }
         }
         write!(output, "\n")?;
         write!(output, "{}", cursor::Goto(1, y as u16 + 1))?;
     }
     output.flush()?;
-    
+
     Ok(())
 }
 
 // ----------  game_board  ----------------
 
 struct BoardCursor {
-    x: i32,y: i32
+    x: i32,
+    y: i32,
 }
 impl BoardCursor {
-    fn new() -> BoardCursor {BoardCursor { x: 0, y: 0 }}
-    fn up(&mut self){
-        if self.y == 0 {self.y = 7} else {self.y -= 1};
+    fn new() -> BoardCursor {
+        BoardCursor { x: 0, y: 0 }
     }
-    fn right(&mut self){
-        if self.x == 7 {self.x = 0} else {self.x += 1};
+    fn up(&mut self) {
+        if self.y == 0 {
+            self.y = 7
+        } else {
+            self.y -= 1
+        };
     }
-    fn down(&mut self){
-        if self.y == 7 {self.y = 0} else {self.y += 1};
+    fn right(&mut self) {
+        if self.x == 7 {
+            self.x = 0
+        } else {
+            self.x += 1
+        };
     }
-    fn left(&mut self){
-        if self.x == 0 {self.x = 7} else {self.x -= 1};
+    fn down(&mut self) {
+        if self.y == 7 {
+            self.y = 0
+        } else {
+            self.y += 1
+        };
+    }
+    fn left(&mut self) {
+        if self.x == 0 {
+            self.x = 7
+        } else {
+            self.x -= 1
+        };
     }
 }
 
@@ -149,16 +173,16 @@ fn game_screen(output: &mut TermOut, input: &mut std::io::Stdin) -> std::io::Res
     let mut board_cursor = BoardCursor::new();
 
     let is_end = |board: &mut Board| -> bool {
-        let is_now_pass =  if board.put_able() == 0 {true} else {false}; 
+        let is_now_pass = if board.put_able() == 0 { true } else { false };
         board.next_turn ^= 1;
-        let is_next_pass = if board.put_able() == 0 {true} else {false};
+        let is_next_pass = if board.put_able() == 0 { true } else { false };
         board.next_turn ^= 1;
         is_now_pass && is_next_pass
     };
 
     use std::time;
     let put = |board: &mut Board, y: i32, x: i32| {
-        
+        let now = time::Instant::now();
         if is_end(board) {
             eprintln!("End!");
             return;
@@ -168,34 +192,43 @@ fn game_screen(output: &mut TermOut, input: &mut std::io::Stdin) -> std::io::Res
             board.next_turn ^= 1;
         }
         if board.next_turn == Board::BLACK {
-            //let re_put = board.put_eval_one_simple();
-            let re_put = put_eval_zero_simple(board);//board.put_piece_from_coord(y, x); //put_eval_one_simple(board);
+            //let re_put = put_eval_one_simple(board);
+            //let re_put = put_eval_zero_simple(board);
+            let re_put = board.put_piece_from_coord(y, x); 
+            //let re_put = board.put_piece(end_game_full_solver_nega_alpha_move_ordering(&board)); 
 
             if let Err(PutPieceErr::NoValidPlacement) = re_put {
                 eprintln!("Err!");
                 return;
             }
         } else {
-            let now = time::Instant::now();
             let re_put;
-            if board.bit_board[Board::BLACK].count_ones() + board.bit_board[Board::WHITE].count_ones() > 47 {
-                re_put = board.put_piece(end_game_full_solver_negamax(&board));
+            let depth_search =
+                64 - (board.bit_board[0].count_ones() + board.bit_board[1].count_ones());
+            eprintln!("depth: {}", depth_search);
+            if depth_search <= 20 {
+                re_put = board.put_piece(end_game_full_solver_nega_alpha_move_ordering(&board));
             } else {
                 re_put = put_eval_one_simple(board);
+                // re_put = put_random_piece(board);
             }
-            eprintln!("{:?}", now.elapsed());
+
             if let Err(PutPieceErr::NoValidPlacement) = re_put {
                 eprintln!("Err!");
                 return;
             }
         }
 
+        let end = now.elapsed().as_secs_f64();
+        eprintln!("time: {}s, nps: {}", end, unsafe {
+            ai::tcount as f64 / end
+        });
     };
     print_board(&board, board_cursor.y, board_cursor.x, output)?;
     for evt in input.events() {
         match evt? {
             // Ctrl-cでプログラム終了
-            Event::Key(Key::Ctrl('c')) =>  {
+            Event::Key(Key::Ctrl('c')) => {
                 return Ok(());
             }
             Event::Key(Key::Char(key_char)) => {
@@ -207,76 +240,166 @@ fn game_screen(output: &mut TermOut, input: &mut std::io::Stdin) -> std::io::Res
                     'x' => {
                         write!(output, "\nsolveing...\n")?;
                         put(&mut board, board_cursor.y, board_cursor.x);
-                    },
+                        // for _ in 0..64 {
+                        //     put(&mut board, board_cursor.y, board_cursor.x);
+                        //     print_board(&board, board_cursor.y, board_cursor.x, output)?;
+                        // }
+                    }
                     'p' => board.next_turn ^= 1,
                     'q' => return Ok(()),
-                    _ => ()
+                    _ => (),
                 }
                 print_board(&board, board_cursor.y, board_cursor.x, output)?;
             }
-            _ => ()
+            _ => (),
         }
     }
     Ok(())
 }
 
-pub fn print_board(board: &Board, y_now: i32, x_now: i32, output: &mut TermOut) -> std::io::Result<()>{
+pub fn print_board(
+    board: &Board,
+    y_now: i32,
+    x_now: i32,
+    output: &mut TermOut,
+) -> std::io::Result<()> {
     init_terminal(output)?;
-    write!(output, "black: {}\n", board.bit_board[Board::BLACK].count_ones())?;
+    write!(
+        output,
+        "black: {}\n",
+        board.bit_board[Board::BLACK].count_ones()
+    )?;
 
     write!(output, "{}", cursor::Goto(1, 2))?;
-    write!(output, "white: {}\n", board.bit_board[Board::WHITE].count_ones())?;
+    write!(
+        output,
+        "white: {}\n",
+        board.bit_board[Board::WHITE].count_ones()
+    )?;
 
     write!(output, "{}", cursor::Goto(1, 3))?;
     for y in 0..8 {
         for x in 0..8 {
             let value: char = {
-                if y == y_now && x == x_now {'+'}
-                else {
+                if y == y_now && x == x_now {
+                    '+'
+                } else {
                     let mask: u64 = 1 << y * 8 + x;
                     let put_able_bit = board.put_able();
-                    
-                    if put_able_bit & mask != 0 {'*'}
-                    else if board.bit_board[0] & mask != 0 {'●'}
-                    else if board.bit_board[1] & mask != 0 {'○'}
-                    else {'.'}
+
+                    if put_able_bit & mask != 0 {
+                        '*'
+                    } else if board.bit_board[0] & mask != 0 {
+                        '●'
+                    } else if board.bit_board[1] & mask != 0 {
+                        '○'
+                    } else {
+                        '.'
+                    }
                 }
             };
             write!(output, "{} ", value)?;
         }
 
         write!(output, "\n")?;
-        write!(output, "{}", cursor::Goto(1, y as u16 +4))?;
-        
+        write!(output, "{}", cursor::Goto(1, y as u16 + 4))?;
     }
     write!(output, "{}", cursor::Goto(1, 13))?;
-    write!(output, "{} turn", if board.next_turn == 0 {"Black"} else {"White"})?;
+    write!(
+        output,
+        "{} turn",
+        if board.next_turn == 0 {
+            "Black"
+        } else {
+            "White"
+        }
+    )?;
 
     output.flush()?;
     Ok(())
 }
 
-
-pub fn start() -> std::io::Result<()>{
-    
+pub fn start() -> std::io::Result<()> {
     let mut stdin: std::io::Stdin = stdin();
-    let mut output = 
-    AlternateScreen::from( stdout().into_raw_mode().unwrap());
+    let mut output = AlternateScreen::from(stdout().into_raw_mode().unwrap());
 
     let title_screen_option = title_screen(&mut output, &mut stdin)?;
     match title_screen_option {
         TitleScreenOption::Start => {
             game_screen(&mut output, &mut stdin)?;
-        },
+        }
         TitleScreenOption::Exit => {
             return Ok(());
-        },
+        }
         _ => {
             return Ok(());
         }
     }
 
     Ok(())
+}
+
+// ---
+
+fn put_piece_eval(board: &mut Board) -> i32 {
+    let player_mobility = board.put_able().count_ones() as i32;
+    board.next_turn = board.next_turn ^ 1;
+    let opponent_mobility = board.put_able().count_ones() as i32;
+    board.next_turn = board.next_turn ^ 1;
+    player_mobility - opponent_mobility
+}
+
+fn put_piece_eval_from_board_pattern() {
+
+}
+
+fn learn_eval_from_board_pattern() {
+    let board_pattern = vec![vec![vec![0i64; 59049]; 3]; 30];
+    /*
+    pattern [30][3][3^10]
+    60手を、30に分ける
+    確かめるパターンは、3つ
+    パターン数は、3通りの箇所が10箇所あるから、3^10
+
+
+    ----------
+    |........|
+    |X.XXXX.X|
+    |XXXXXXXX|
+    |XXXXXXXX|
+    |XXXXXXXX|
+    |XXXXXXXX|
+    |XXXXXXXX|
+    |XXXXXXXX|
+    ----------
+
+
+    and
+
+    ----------
+    |X......X|
+    |XX....XX|
+    |XXXXXXXX|
+    |XXXXXXXX|
+    |XXXXXXXX|
+    |XXXXXXXX|
+    |XXXXXXXX|
+    |XXXXXXXX|
+    ----------
+
+    and
+
+    ----------
+    |.XXXXXXX|
+    |X.XXXXXX|
+    |XX.XXXXX|
+    |XXX.XXXX|
+    |XXXX.XXX|
+    |XXXXX.XX|
+    |XXXXXX.X|
+    |XXXXXXX.|
+    ----------
+    */
 }
 
 fn main() -> std::io::Result<()> {
